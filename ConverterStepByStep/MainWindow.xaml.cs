b3de4fbd22b6e9129ca68d5aacb829133367b30b
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2025-01-27 18:49:27 +0100

    Add project files.

---
+++ ConverterStepByStep/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ConverterStepByStep;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	private readonly bool _catchErrors = true;
+
+	public MainWindow() 
+		=> InitializeComponent();
+
+	private void OnBtnConvertClick(object sender, RoutedEventArgs e) 
+		=> ConvertNumber();
+
+	private void Window_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			ConvertNumber();
+		}
+	}
+
+	private void ConvertNumber()
+	{
+		try
+		{
+			int number = int.Parse(BaseNumber.Text);
+			TxtBinSteps.Text = TxtOctSteps.Text = TxtHexSteps.Text = string.Empty;
+			ConvertToBin(number);
+			ConvertToOct(number);
+			ConvertToHex(number);
+		}
+		catch (Exception ex) when (_catchErrors)
+		{
+			BaseNumber.Text = "ERRORRRRRRR!!!";
+			_ = MessageBox.Show(ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+	}
+
+	private void ConvertToBin(int number)
+	{
+		string result = string.Empty;
+		int counter = 0;
+		do
+		{
+			TxtBinSteps.Text += $"{number} / 2 = ";
+			int remainder = number % 2;
+			result = remainder.ToString() + result;
+			number /= 2;
+			TxtBinSteps.Text += $"{number} reszta {remainder}\n";
+
+			counter++;
+			if (counter % 4 == 0)
+			{
+				result = " " + result;
+			}
+		} while (number > 0);
+		BinNumber.Text = result;
+	}
+
+	private void ConvertToOct(int number)
+	{
+		string result = string.Empty;
+		int counter = 0;
+		do
+		{
+			TxtOctSteps.Text += $"{number} / 8 = ";
+			int remainder = number % 8;
+			result = remainder.ToString() + result;
+			number /= 8;
+			TxtOctSteps.Text += $"{number} reszta {remainder}\n";
+			counter++;
+			if (counter % 3 == 0)
+			{
+				result = " " + result;
+			}
+		} while (number > 0);
+		OctNumber.Text = result;
+	}
+
+	private void ConvertToHex(int number)
+	{
+		string result = string.Empty;
+		int counter = 0;
+		do
+		{
+			TxtHexSteps.Text += $"{number} / 16 = ";
+			int remainder = number % 16;
+			number /= 16;
+			TxtHexSteps.Text += $"{number} reszta {remainder}";
+
+			if (remainder > 9)
+			{
+				string letter = remainder switch
+				{
+					10 => "A",
+					11 => "B",
+					12 => "C",
+					13 => "D",
+					14 => "E",
+					15 => "F",
+					_ => string.Empty
+				};
+
+				result = letter + result;
+				TxtHexSteps.Text += $" ({letter})\n";
+			}
+			else
+			{
+				result = remainder.ToString() + result;
+				TxtHexSteps.Text += Environment.NewLine;
+			}
+			counter++;
+			if (counter % 4 == 0)
+			{
+				result = " " + result;
+			}
+		} while (number > 0);
+		HexNumber.Text = result;
+	}
+}
---
+++ ImageBrowser/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace ImageBrowser;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	private readonly Random _random = new();
+	private readonly string[] _files = Directory.GetFiles(Environment.CurrentDirectory, "*", SearchOption.AllDirectories);
+
+	private int _imgNumber;
+
+	private enum ActionType
+	{
+		previous, random, next
+	}
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		NewImage(ActionType.random);
+		//_files.OrderBy(x => x);
+		_files = _files.OrderBy(x => x).ToArray();
+	}
+
+	private void BtnPrevious_Click(object sender, RoutedEventArgs e)
+		=> NewImage(ActionType.previous);
+
+	private void BtnRandom_Click(object sender, RoutedEventArgs e)
+		=> NewImage(ActionType.random);
+
+	private void BtnNext_Click(object sender, RoutedEventArgs e)
+		=> NewImage(ActionType.next);
+
+	private void Window_KeyDown(object sender, KeyEventArgs e)
+	{
+		switch (e.Key)
+		{
+			case Key.Left:
+				NewImage(ActionType.previous);
+				break;
+			case Key.Space:
+				NewImage(ActionType.random);
+				break;
+			case Key.Right:
+				NewImage(ActionType.next);
+				break;
+			case Key.Escape:
+				Close();
+				break;
+		}
+	}
+
+	private void ImgViewer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		=> NewImage(ActionType.random);
+
+	private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+	{
+		ActionType actionType = e.Delta > 0 ? ActionType.previous : ActionType.next;
+		NewImage(actionType);
+	}
+
+	private void NewImage(ActionType actionType)
+	{
+		BitmapImage image = new();
+		image.BeginInit();
+
+		switch (actionType)
+		{
+			case ActionType.previous:
+				_imgNumber--;
+				if (_imgNumber < 0)
+				{
+					_imgNumber = _files.Length - 1;
+				}
+				image.UriSource = new Uri(_files[_imgNumber]);
+				break;
+			case ActionType.random:
+				_imgNumber = _random.Next(_files.Length);
+				image.UriSource = new Uri(_files[_imgNumber]);
+				break;
+			case ActionType.next:
+				_imgNumber++;
+				if (_imgNumber >= _files.Length)
+				{
+					_imgNumber = 0;
+				}
+				image.UriSource = new Uri(_files[_imgNumber]);
+				break;
+		}
+
+		image.EndInit();
+		ImgViewer.Source = image;
+		Title = image.UriSource.ToString();
+	}
+}
