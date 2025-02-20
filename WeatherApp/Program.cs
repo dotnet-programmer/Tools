5db96e5fc487662df1f5b685b4ad67b4b9659b80
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2025-02-20 17:16:58 +0100

    WeatherApp

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using WeatherApp;
+
+ConsoleColor defaultColor = Console.ForegroundColor;
+int defaultHeight = Console.WindowHeight;
+
+var weatherList = WeatherApi.GetWeather("Gdańsk", "Kielce");
+
+Console.WindowHeight = defaultHeight + 10;
+
+foreach (var weather in weatherList)
+{
+	Console.ForegroundColor = ConsoleColor.White;
+	Console.WriteLine($"\t  {weather.name.ToUpper()}\n");
+	Console.ForegroundColor = defaultColor;
+
+	Console.WriteLine("Aktualna Odczuwalna   Max     Min");
+	Console.ForegroundColor = ConsoleColor.Green;
+	Console.Write($" {weather.main.temp:f2}\x00B0C    ");
+	Console.ForegroundColor = ConsoleColor.Yellow;
+	Console.Write($"{weather.main.feels_like:f2}\x00B0C   ");
+	Console.ForegroundColor = ConsoleColor.Red;
+	Console.Write($"{weather.main.temp_max:f2}\x00B0C  ");
+	Console.ForegroundColor = ConsoleColor.Blue;
+	Console.WriteLine($"{weather.main.temp_min:f2}\x00B0C");
+	Console.ForegroundColor = defaultColor;
+
+	Console.WriteLine($"\nPogoda: {weather.weather[0].main} - {weather.weather[0].description}");
+	Console.WriteLine($"\nPrędkość wiatru: {weather.wind.speed:f2}m/s = {MpsToKph(weather.wind.speed):f2}km/h");
+	Console.WriteLine($"\nCiśnienie: {weather.main.pressure}hPa");
+	Console.WriteLine($"\nWilgotność: {weather.main.humidity}%");
+
+	Console.WriteLine(Environment.NewLine);
+	Console.WriteLine(Environment.NewLine);
+}
+
+Console.ReadLine();
+
+static double MpsToKph(double mps)
+	=> mps * 3.6;
---
+++ WeatherApp/WeatherApi.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+
+namespace WeatherApp;
+
+internal class WeatherApi
+{
+	//private const string _apiAddressId = "https://api.openweathermap.org/data/2.5/weather?id=";
+	private const string _apiAddressCity = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+	private const string _apiKey = "&appid=f85b83051e492adccf5a57e6062e62ff";
+	private const string _parameters = "&units=metric&lang=pl";
+
+	public static List<WeatherJson> GetWeather(params string[] cities)
+	{
+		List<WeatherJson> result = new(cities.Length);
+
+		foreach (var item in cities)
+		{
+			var weather = JsonSerializer.Deserialize<WeatherJson>(GetWeatherJson($"{_apiAddressCity}{item}{_apiKey}{_parameters}").Result);
+
+			if (weather.cod != 200)
+			{
+				Console.WriteLine($"Błąd dla miejscowości: {item}");
+				break;
+			}
+
+			result.Add(weather);
+		}
+
+		return result;
+	}
+
+	private static async Task<string> GetWeatherJson(string url)
+	{
+		string json = string.Empty;
+		try
+		{
+			HttpClient httpClient = new();
+			var response = await httpClient.GetAsync(url);
+
+			if (!response.IsSuccessStatusCode)
+			{
+				throw new Exception(response.RequestMessage.ToString());
+			}
+
+			json = await response.Content.ReadAsStringAsync();
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Http.Get method error: {ex.Message}");
+		}
+
+		return json;
+	}
+}
