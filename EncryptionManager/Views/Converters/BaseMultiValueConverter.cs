0a31046dc2501e15f1bc8d9f7e6cc47fc17e6a2c
Michal L. <26222619+dotnet-programmer@users.noreply.github.com>
2025-07-15 18:02:54 +0200

    Add view Converters

---
+++ EncryptionManager/Views/Converters/BaseMultiValueConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EncryptionManager.Views.Converters;
+
+/// <summary>
+/// Base value converter to reduce rewriting a bunch of base code
+/// </summary>
+/// <typeparam name="T">Converter class</typeparam>
+internal abstract class BaseMultiValueConverter<T> : MarkupExtension, IMultiValueConverter where T : class, new()
+{
+	private static T? _converter;
+
+	#region MarkupExtension Members
+	/// <summary>
+	/// Makes the converter available to use in xaml
+	/// </summary>
+	/// <param name="serviceProvider"></param>
+	/// <returns></returns>
+	public override object ProvideValue(IServiceProvider serviceProvider)
+		=> _converter ?? (_converter = new T());
+	#endregion MarkupExtension Members
+
+	#region IMultiValueConverter Members
+	/// <summary>
+	/// The convert method to override
+	/// </summary>
+	/// <param name="values">The values to convert</param>
+	/// <param name="targetType"></param>
+	/// <param name="parameter"></param>
+	/// <param name="culture"></param>
+	/// <returns></returns>
+	public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+	public abstract object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture);
+	#endregion IMultiValueConverter Members
+}
---
+++ EncryptionManager/Views/Converters/BoolToColorConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EncryptionManager.Views.Converters;
+
+[ValueConversion(typeof(bool), typeof(SolidColorBrush))]
+internal class BoolToColorConverter : BaseValueConverter<BoolToColorConverter>
+{
+	public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		=> System.Convert.ToBoolean(value) ? Brushes.ForestGreen : Brushes.IndianRed;
+
+	public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		=> throw new NotImplementedException();
+}
